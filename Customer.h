59dffbab8d12e07a17d255a686105459c58e6de9
openoneforme <55337524+openoneforme@users.noreply.github.com>
2021-01-14 18:09:30 +0800

    Create Customer.h

---
+++ Customer.h
@@ -0,0 +1,15 @@
+#include <string>
+using namespace std;
+
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+class Customer
+{
+public:
+	string CustomerName, CustomerAddress, CustomerTel, CustomerEmail, AccountID, NRIC;
+	int CustomerID;
+	bool Gender;
+};
+
+#endif
